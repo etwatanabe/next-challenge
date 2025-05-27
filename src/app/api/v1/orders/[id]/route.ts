8f340739625e938b886b9f4e93478c3311db2939
Eduardo Takeshi <wattksh@gmail.com>
2025-05-27 17:28:38 -0300

    feat: implement GET and POST methods for order management in route handler

---
+++ src/app/api/v1/orders/[id]/route.ts
@@ -0,0 +1,28 @@
+import { addItemToOrderUseCase } from "@/factories/orderUseCaseFactory";
+import { NextRequest, NextResponse } from "next/server";
+
+//PUT: Add item to an order by ID
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const { id } = await params;
+
+    const { productId, quantity } = await request.json();
+
+    const sellerId = request.headers.get("X-User-Id");
+
+    const useCase = addItemToOrderUseCase;
+
+    const order = await useCase.execute(id, productId, quantity, sellerId!);
+
+    return NextResponse.json(order, { status: 200 });
+  } catch (error) {
+    console.error("Error adding item to order:", error);
+    return NextResponse.json(
+      { error: "Failed to add item to order" },
+      { status: 500 }
+    );
+  }
+}
---
+++ src/app/api/v1/orders/route.ts
@@ -0,0 +1,51 @@
+import { CreateOrderDTO } from "@/core/dtos/order/CreateOrderDTO";
+import {
+  listOrdersUseCase,
+  createOrderUseCase,
+} from "@/factories/orderUseCaseFactory";
+import { NextRequest, NextResponse } from "next/server";
+
+// GET: List all orders for a seller
+export async function GET(request: NextRequest) {
+  try {
+    const sellerId = request.headers.get("X-User-Id");
+
+    const useCase = listOrdersUseCase;
+
+    const orders = await useCase.execute(sellerId!);
+
+    return NextResponse.json(orders, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching orders:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch orders" },
+      { status: 500 }
+    );
+  }
+}
+
+// POST: Create a new order
+export async function POST(request: NextRequest) {
+  try {
+    const body = await request.json();
+
+    const sellerId = request.headers.get("X-User-Id");
+
+    const data: CreateOrderDTO = {
+      sellerId: sellerId!,
+      items: body.items,
+    };
+
+    const useCase = createOrderUseCase;
+
+    const order = await useCase.execute(data);
+
+    return NextResponse.json(order, { status: 201 });
+  } catch (error) {
+    console.error("Error creating order:", error);
+    return NextResponse.json(
+      { error: "Failed to create order" },
+      { status: 500 }
+    );
+  }
+}
