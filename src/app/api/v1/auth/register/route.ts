b994fb1162c141410118288cd6968b26ed73ba81
Eduardo Takeshi <wattksh@gmail.com>
2025-05-27 00:13:04 -0300

    refactor: fix import and  remove spaces

--- src/app/api/v1/auth/register/route.ts
+++ src/app/api/v1/auth/register/route.ts
@@ -1,4 +1,4 @@
-import { CreateSellerUseCase } from "@/core/usecases/seller/CreateSellerUseCase";
+import { CreateSellerUseCase } from "@/core/usecases/seller/RegisterSellerUseCase";
 import { PrismaSellerRepository } from "@/infra/prisma/PrismaSellerRepository";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -7,16 +7,16 @@ const repository = new PrismaSellerRepository();
 export async function POST(request: NextRequest) {
   try {
     const { name, email, password } = await request.json();
-    
+
     if (!name || !email || !password) {
       return NextResponse.json(
         { error: "Missing required fields" },
         { status: 400 }
       );
     }
-    
+
     const useCase = new CreateSellerUseCase(repository);
-    
+
     const seller = await useCase.execute({
       name,
       email,
@@ -25,12 +25,14 @@ export async function POST(request: NextRequest) {
       orders: [],
     });
 
-    return NextResponse.json({
-      id: seller.id,
-      name: seller.name,
-      email: seller.email
-    }, { status: 201 }); // 201 Created é mais apropriado para criação
-    
+    return NextResponse.json(
+      {
+        id: seller.id,
+        name: seller.name,
+        email: seller.email,
+      },
+      { status: 201 }
+    );
   } catch (error) {
     console.error("Error creating seller:", error);
     return NextResponse.json(
