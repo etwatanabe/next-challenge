d6e8af88e98ff35dc2753579328fd2701478286b
Eduardo Takeshi <wattksh@gmail.com>
2025-05-27 00:04:39 -0300

    fix: update IProductRepository methods to include sellerId for findByName and findAllBySellerId

--- src/core/domain/repositories/IProductRepository.ts
+++ src/core/domain/repositories/IProductRepository.ts
@@ -3,8 +3,8 @@ import { Product } from "@/core/domain/entities/Product";
 export interface IProductRepository {
   create(product: Product): Promise<Product>;
   findById(id: string): Promise<Product | null>;
-  findByName(name: string): Promise<Product | null>;
-  findAll(): Promise<Product[]>;
+  findByName(name: string, sellerId: string): Promise<Product | null>;
+  findAllBySellerId(sellerId: string): Promise<Product[]>;
   delete(id: string): Promise<void>;
   update(product: Product): Promise<Product>;
 }
