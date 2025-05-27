861087a40470d71a69d5aa8a550ca4dfcf8c1d0c
Eduardo Takeshi <wattksh@gmail.com>
2025-05-27 17:28:07 -0300

    feat: add productRepository to createOrderUseCase and addItemToOrderUseCase for improved order management

--- src/factories/orderUseCaseFactory.ts
+++ src/factories/orderUseCaseFactory.ts
@@ -1,11 +1,27 @@
+import { AddItemToOrderUseCase } from "@/core/usecases/order/AddItemToOrderUseCase";
+import { CreateOrderUseCase } from "@/core/usecases/order/CreateOrderUseCase";
 import { ListOrdersUseCase } from "@/core/usecases/order/ListOrdersUseCase";
 import { PrismaOrderRepository } from "@/infra/prisma/PrismaOrderRepository";
+import { PrismaProductRepository } from "@/infra/prisma/PrismaProductRepository";
 import { PrismaSellerRepository } from "@/infra/prisma/PrismaSellerRepository";
 
 const orderRepository = new PrismaOrderRepository();
+const productRepository = new PrismaProductRepository();
 const sellerRepository = new PrismaSellerRepository();
 
 export const listOrdersUseCase = new ListOrdersUseCase(
   orderRepository,
+  sellerRepository
+);
+
+export const createOrderUseCase = new CreateOrderUseCase(
+  orderRepository,
+  productRepository,
+  sellerRepository
+);
+
+export const addItemToOrderUseCase = new AddItemToOrderUseCase(
+  orderRepository,
+  productRepository,
   sellerRepository
 );
