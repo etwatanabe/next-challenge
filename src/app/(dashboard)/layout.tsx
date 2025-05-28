9addf362a8a403db613e7cdefade4dbb3cff745a
Eduardo Takeshi <wattksh@gmail.com>
2025-05-28 18:26:59 -0300

    refactor: adjust formatting

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,22 +1,20 @@
-import './globals.css'
-import { SessionProvider } from '@/contexts/SessionProvider'
-import { getServerSession } from 'next-auth'
-import { authOptions } from '@/utils/auth'
+import "@/app/globals.css";
+import { SessionProvider } from "@/contexts/SessionProvider";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/utils/auth";
 
 export default async function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-  const session = await getServerSession(authOptions)
+  const session = await getServerSession(authOptions);
 
   return (
     <html lang="pt-br">
       <body>
-        <SessionProvider session={session}>
-          {children}
-        </SessionProvider>
+        <SessionProvider session={session}>{children}</SessionProvider>
       </body>
     </html>
-  )
+  );
 }
