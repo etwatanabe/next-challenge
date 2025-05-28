42be730f503cfc29637479e4363a0e47f71a10dc
Eduardo Takeshi <wattksh@gmail.com>
2025-05-28 00:03:31 -0300

    refactor: moving register route

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -7,14 +7,7 @@ const repository = new PrismaSellerRepository();
 export async function POST(request: NextRequest) {
   try {
     const { name, email, password } = await request.json();
-
-    if (!name || !email || !password) {
-      return NextResponse.json(
-        { error: "Missing required fields" },
-        { status: 400 }
-      );
-    }
-
+    
     const useCase = new CreateSellerUseCase(repository);
 
     const seller = await useCase.execute({
