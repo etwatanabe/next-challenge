036a045880937e512a98b7948c7cdb2759223238
Eduardo Takeshi <wattksh@gmail.com>
2025-05-28 17:18:59 -0300

    feat: add Stripe

---
+++ src/utils/stripe.ts
@@ -0,0 +1,12 @@
+import Stripe from "stripe";
+
+if (!process.env.STRIPE_SECRET_KEY) {
+  throw new Error("Stripe secret key is not defined in environment variables.");
+}
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: "2025-05-28.basil",
+  typescript: true,
+});
+
+export default stripe;
