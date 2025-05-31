c0b2be88f4bd5613d59bc9c1155a483099200bfd
Eduardo Takeshi <wattksh@gmail.com>
2025-05-31 15:54:17 -0300

    feat: remove status property from CreateOrderDTO

--- src/core/dtos/order/CreateOrderDTO.ts
+++ src/core/dtos/order/CreateOrderDTO.ts
@@ -1,8 +1,5 @@
-import { OrderStatus } from "@/core/domain/enums/OrderStatus";
-
 export type CreateOrderDTO = {
   productId: string;
-  status: OrderStatus;
   customerName: string;
   customerEmail: string;
   customerPhone: string;
