0ba7a6326de4d35c8ef801075b68c03fbb79a144
Eduardo Takeshi <wattksh@gmail.com>
2025-05-31 17:32:34 -0300

    refactor: remove GetOrderByIdPublicUseCase and related endpoint for public access

--- src/factories/orderUseCaseFactory.ts
+++ src/factories/orderUseCaseFactory.ts
@@ -4,7 +4,6 @@ import { ListOrdersUseCase } from "@/core/usecases/order/ListOrdersUseCase";
 import { PrismaOrderRepository } from "@/infra/prisma/PrismaOrderRepository";
 import { PrismaProductRepository } from "@/infra/prisma/PrismaProductRepository";
 import { PrismaSellerRepository } from "@/infra/prisma/PrismaSellerRepository";
-import { GetOrderByIdPublicUseCase } from "@/core/usecases/order/GetOrderByIdPublicUseCase";
 import { CompleteOrderUseCase } from "@/core/usecases/order/CompleteOrderUseCase";
 
 const orderRepository = new PrismaOrderRepository();
@@ -25,10 +24,6 @@ export const createOrderUseCase = new CreateOrderUseCase(
 export const getOrderByIdUseCase = new GetOrderByIdUseCase(
   orderRepository,
   sellerRepository
-);
-
-export const getOrderByIdPublicUseCase = new GetOrderByIdPublicUseCase(
-  orderRepository
 );
 
 export const completeOrderUseCase = new CompleteOrderUseCase(orderRepository);
