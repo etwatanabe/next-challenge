6e4adebdd2ade0bf99c3b1b3d959c2552596db56
Takeshi <wattksh@gmail.com>
2025-05-31 11:21:14 -0300

    refactor: remove unused PUT endpoint for adding items to an order

--- src/app/api/v1/(dashboard)/dashboard/orders/[id]/route.ts
+++ src/app/api/v1/(dashboard)/dashboard/orders/[id]/route.ts
@@ -1,34 +1,5 @@
-import {
-  addItemToOrderUseCase,
-  getOrderByIdUseCase,
-} from "@/factories/orderUseCaseFactory";
+import { getOrderByIdUseCase } from "@/factories/orderUseCaseFactory";
 import { NextRequest, NextResponse } from "next/server";
-
-//PUT: Add item to an order by ID
-export async function PUT(
-  request: NextRequest,
-  { params }: { params: { id: string } }
-) {
-  try {
-    const { id } = await params;
-
-    const { productId, quantity } = await request.json();
-
-    const sellerId = request.headers.get("X-User-Id");
-
-    const useCase = addItemToOrderUseCase;
-
-    const order = await useCase.execute(id, productId, quantity, sellerId!);
-
-    return NextResponse.json(order, { status: 200 });
-  } catch (error) {
-    console.error("Error adding item to order:", error);
-    return NextResponse.json(
-      { error: "Internal Server Error" },
-      { status: 500 }
-    );
-  }
-}
 
 // GET: Get an order by ID
 export async function GET(
